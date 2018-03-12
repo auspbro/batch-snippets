da9009f50f23ad7c3596697d5f2304902edc31d9
Ryan Xue <auspbro@gmail.com>
2018-03-12 23:37:42 +0800

    Add comment

--- 字符串处理/substr/substr.bat
+++ 字符串处理/substr/substr.bat
@@ -1,8 +1,8 @@
 @echo on
 
-:: 使用 for 语句跳过前三行，以 '(' 和 ',' 为 delims截取 log 文件中第二个 tokens
+:: 使用 for 语句跳过前三行，以 '(' 和 ',' 为 delims 截取 log 文件中第二个 tokens
 for /f "skip=3 delims=(, tokens=2" %%i in (uu1_result_motor_for_0414.bat) do echo %%i
 pause
 
 for /f "skip=3 tokens=4 delims=) " %%j in (uu1_result_motor_for_0414.bat) do echo %%j
-pause  
+pause  ·
