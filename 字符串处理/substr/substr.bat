5283fa7ba9482d40ee48d8b54c3afad4dca35137
auspbro <auspbro@gmail.com>
2018-03-13 21:22:39 +0800

    Update substr.bat

--- 字符串处理/substr/substr.bat
+++ 字符串处理/substr/substr.bat
@@ -5,4 +5,4 @@ for /f "skip=3 delims=(, tokens=2" %%i in (uu1_result_motor_for_0414.bat) do ech
 pause
 
 for /f "skip=3 tokens=4 delims=) " %%j in (uu1_result_motor_for_0414.bat) do echo %%j
-pause  ·
+pause
